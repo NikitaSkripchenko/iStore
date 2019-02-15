a9ff3747415e822880b9ea31b41fa4b6b368f0f0
Nikita Skrypchenko <frickadelka456@gmail.com>
2019-02-15 22:50:58 +0200

    Initial Commit

---
+++ iStore/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iStore
+//
+//  Created by Nikita Skrypchenko  on 2/15/19.
+//  Copyright © 2019 Nikita Skrypchenko . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
